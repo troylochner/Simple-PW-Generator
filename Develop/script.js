6782d758ec205e62b55f422f7ede196476b513f9
troylochner <troy.lochner@gmail.com>
2020-08-16 13:31:50 -0400

    first code commit; began generate PW function. Early prompt screens. Verified working

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,49 @@
+
+/*
+--- WORKING STRATEGY---
+
+DEFINE 4 BUCKETS OF characters : lower ; upper ; numeric ; special char
+theLower
+theUpper
+theNumber
+theSpecial
+
+PROMPT USER FOR PW LENGTH
+
+PROMPT USER FOR SPECIAL SELECTORS
+  VALIDATE USER INPUT -- REQUIRE 1 CHAR PER BUCKET
+
+
+
+*/
+
+//DEFINE STARTING BUCKET OF CHARACTERS TO SELECT FROM
+var charBucket = {
+  theLower:"abcdefghijklmnopqrstuvwxyz",
+  theUpper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  theNumber:"0123456789",
+  theSpecial: "!#$%&*-:;<=>?@[^_`{|}~"
+}
+
+
+
+
+//GENERATE PASSWORD PROCESS
+function generatePassword() {
+  alert("The button click works");
+  
+  //EARLY PROMPTS - REFINE LATER. 
+  var myLength = prompt("Please enter a password length \n Note : Length must be between 8-128");
+  var numUpper = prompt("How many uppercase characters are required? ( min: 1 )") ;
+  var numNumber = prompt("How many numeric characters are required? ( min: 1 )") ;
+  var numSpecial = prompt("How many special characters are required? ( min: 1 ) ") ;
+
+}
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
