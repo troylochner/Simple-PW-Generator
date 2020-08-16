58e3354769c4c995cbf18b874b32791858aa677c
troylochner <troy.lochner@gmail.com>
2020-08-16 16:16:12 -0400

    added easy confirmation at end

--- script.js
+++ script.js
@@ -47,6 +47,8 @@ function generatePassword() {
 
   pwGen(myLength,numLower,numUpper,numNumber,numSpecial);
 
+  return "butt";
+
 }
 
 /*---------------------------------------------------------------/*/
